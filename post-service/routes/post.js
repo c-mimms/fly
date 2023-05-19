23b1c15d5537e7fe5c5f98adf98ea8dc4def23d8
Christopher Mimms <cmimms@squareup.com>
2023-05-19 01:49:36 -0400

    Modify posts page.

--- post-service/routes/post.js
+++ post-service/routes/post.js
@@ -4,7 +4,7 @@ import { getPost, getPosts } from '../db/posts.js';
 
 const router = Router();
 
-router.get('/:id', getSinglePostHandler);
+router.get('/:id', getSinglePostHandler2);
 
 
 async function getSinglePostHandler(req, res) {
@@ -23,5 +23,40 @@ async function getSinglePostHandler(req, res) {
     res.status(500).json({ error: 'An error occurred while retrieving the post.' });
   }
 }
+
+
+async function getSinglePostHandler2(req, res) {
+    const { id } = req.params;
+
+    try {
+      const post = await getPost(id);
+      if (!post) return res.status(404).json({ error: "Post not found." });
+
+      post.linkedPosts = await getNestedComments(post.id, 3);
+      res.render('post', { post });
+
+    } catch (error) {
+      console.error('Error retrieving post:', error);
+      res.status(500).json({ error: 'An error occurred while retrieving the post.' });
+    }
+  }
+
+  async function getNestedComments(postId, depth) {
+    if (depth === 0) {
+      return [];
+    }
+
+    // Fetch the post and its replies.
+    const replies = await getPosts({ outgoingLinks: postId });
+
+    // Fetch nested replies recursively.
+    for (const reply of replies) {
+      reply.linkedPosts = await getNestedComments(reply.id, depth - 1);
+    }
+
+    return replies;
+  }
+
+
 
 export { router };
