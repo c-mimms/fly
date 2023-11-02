b0721541c32ccb4fca991946d46fd685a7e20564
Chris Mimms <christek13@gmail.com>
2023-11-01 22:41:08 -0400

    Migrated base-app into own repo.

--- base-app/src/routes/api/api.js
+++
@@ -1,9 +0,0 @@
-// routes/api/
-import { Router } from 'express';
-import { router as postRouter } from './posts.js';
-
-const router = Router();
-
-router.use('/posts', postRouter);
-
-export { router };
